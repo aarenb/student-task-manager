f2108bdac513585a9438c962b9b2ccdef75a397b
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-10-18 13:18:07 +0200

    Change the look of a task-item

--- src/js/components/task-item/task-item.js
+++ src/js/components/task-item/task-item.js
@@ -1,13 +1,47 @@
 const template = document.createElement('template')
+// TODO: Can you break out this css into seperate file?
 template.innerHTML = `
-<style>
-  #main{
-    background: purple;
-    padding: 50px;
+<style> 
+  #container {
+    background: rgb(230, 139, 240);
+    padding: 12px;
+    border: solid black 2px;
+  }
+  #checkAndName {
+    display: flex;
+    align-items: center;
+    background: orange;
+  }
+  #name {
+    margin-left: 8px;
+  }
+  #check {
+    width: 20px;
+    height: 20px;
+  }
+  #dueDate {
+    display: flex;
+    align-items: center;
+    background: green;
+  }
+  #date {
+    margin-left: 8px;
+  }
+  #time {
+    margin-left: 8px;
   }
 </style>
-<div id="main">
-  <p> this is a task </p>
+<div id="container">
+  <div id="checkAndName">
+    <input type="checkbox" id="check"/> 
+    <h3 id="name"> Fix this website </h3>
+  </div>
+  <p id="description"> Very important!! have to get that csn </p>
+  <div id="dueDate">
+  <p> Due by: </p>
+  <p id="date"> 23/10/2023 </p>
+  <p id="time"> 09:00 </p>
+  </div>
 </div>
 `
 
