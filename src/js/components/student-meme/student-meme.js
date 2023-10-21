ef744c558ce7d2dc36aecf6b0707eac1246b415f
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-10-21 09:28:17 +0200

    Make assignRandomMeme method better

--- src/js/components/student-meme/student-meme.js
+++ src/js/components/student-meme/student-meme.js
@@ -30,41 +30,16 @@ customElements.define('student-meme',
         .appendChild(template.content.cloneNode(true))
 
       this.#image = this.shadowRoot.querySelector('img')
-      this.#showRandomImage()
+      this.#assignRandomMeme()
     }
 
     /**
-     * Assigns a random image to the image element.
+     * Assigns a random meme to the image element.
      */
-    #showRandomImage () {
-      const random = Math.floor(Math.random() * 8) // TODO: break out?
+    #assignRandomMeme () {
+      const randomNumber = Math.floor(Math.random() * 11) + 1
 
-      switch (random) {
-        case 0:
-          this.#image.setAttribute('src', '../images/meme1.jpg')
-          break
-        case 1:
-          this.#image.setAttribute('src', '../images/meme2.jpg')
-          break
-        case 2:
-          this.#image.setAttribute('src', '../images/meme3.jpg')
-          break
-        case 3:
-          this.#image.setAttribute('src', '../images/meme4.jpg')
-          break
-        case 4:
-          this.#image.setAttribute('src', '../images/meme5.jpg')
-          break
-        case 5:
-          this.#image.setAttribute('src', '../images/meme6.jpg')
-          break
-        case 6:
-          this.#image.setAttribute('src', '../images/meme7.jpg')
-          break
-        case 7:
-          this.#image.setAttribute('src', '../images/meme8.jpg')
-          break
-      }
+      this.#image.setAttribute('src', `../images/meme${randomNumber}.jpg`)
     }
   }
 )
