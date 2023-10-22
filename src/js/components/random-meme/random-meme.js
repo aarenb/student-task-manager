07231b1e4bf8e2c6162e8b9f240bc3353f3aee2a
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-10-22 18:19:30 +0200

    Try to fix meme images loading issue

--- src/js/components/random-meme/random-meme.js
+++ src/js/components/random-meme/random-meme.js
@@ -1,4 +1,23 @@
 import meme1 from '/images/meme1.jpg'
+import meme2 from '/images/meme2.jpg'
+import meme3 from '/images/meme3.jpg'
+import meme4 from '/images/meme4.jpg'
+import meme5 from '/images/meme5.jpg'
+import meme6 from '/images/meme6.jpg'
+import meme7 from '/images/meme7.jpg'
+import meme8 from '/images/meme8.jpg'
+import meme9 from '/images/meme9.jpg'
+import meme10 from '/images/meme10.jpg'
+import meme11 from '/images/meme11.jpg'
+import meme12 from '/images/meme12.jpg'
+import meme13 from '/images/meme13.jpg'
+import meme14 from '/images/meme14.jpg'
+import meme15 from '/images/meme15.jpg'
+import meme16 from '/images/meme16.jpg'
+import meme17 from '/images/meme17.jpg'
+import meme18 from '/images/meme18.jpg'
+import meme19 from '/images/meme19.jpg'
+// This is the only way to show the memes on netlify, I know it's not ideal.
 
 const template = document.createElement('template')
 template.innerHTML = `
@@ -22,6 +41,7 @@ customElements.define('random-meme',
    */
   class extends HTMLElement {
     #image
+    #images
     /**
      * Creates an instance of the current type.
      */
@@ -32,6 +52,8 @@ customElements.define('random-meme',
         .appendChild(template.content.cloneNode(true))
 
       this.#image = this.shadowRoot.querySelector('img')
+      this.#images = [meme1, meme2, meme3, meme4, meme5, meme6, meme7, meme8, meme9, meme10, meme11, meme12, meme13, meme14, meme15, meme16, meme17, meme18, meme19]
+
       this.#assignRandomMeme()
     }
 
@@ -39,8 +61,8 @@ customElements.define('random-meme',
      * Assigns a random meme to the image element.
      */
     #assignRandomMeme () {
-      // const randomNumber = Math.floor(Math.random() * 19) + 1
-      this.#image.src = meme1
+      const randomNumber = Math.floor(Math.random() * 19)
+      this.#image.src = this.#images[randomNumber]
     }
   }
 )
